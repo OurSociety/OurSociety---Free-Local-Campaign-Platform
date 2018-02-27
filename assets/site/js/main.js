8a2cd44e6e0fa9e9b02153980e913e0f7cb274d4
Teodor Talov <teodor@talov.com>
2018-02-27 14:04:23 -0500

    Fixed WYSIWYG editor system wide

--- assets/site/js/main.js
+++ assets/site/js/main.js
@@ -7,11 +7,11 @@ import 'jdenticon/dist/jdenticon.js'
  * Application
  */
 // import './_datetime' // TODO: Get datetime picker working.
+import './_vue' // VueJS components
 import './_editor' // wysiwyg editor
 import './_video' // video carousel
 import './_onboarding' // onboarding dropdowns
 import './_search' // algolia search
 import './_flash' // flash/toast notifications
 import './_dropdown' // bootstrap dropdown menus
-import './_vue' // VueJS components
 import './_month' // month input polyfill
